e1cd5acd82752f65a83ca94ed1c6144eb112b219
sam <68155940+samuelgecik@users.noreply.github.com>
2026-04-04 20:42:41 +0300

    Change middleware for proxy convention

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,8 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
+export const runtime = "edge";
+
 export default createMiddleware(routing);
 
 export const config = {
